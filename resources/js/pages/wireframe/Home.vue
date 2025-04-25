84eaa300fa7f1d25a6dc05ea3913145d1f1b7675
daurelius <davad.tandjung@gmail.com>
2025-04-25 11:15:00 +0700

    setup: get Montserrat from google fornt

--- resources/js/pages/wireframe/Home.vue
+++ resources/js/pages/wireframe/Home.vue
@@ -2,9 +2,9 @@
 </script>
 
 <template>
-    <div class="place-items-center grid bg-amber-50 h-screen">
-        <div class="place-items-center grid bg-amber-100 rounded-3xl w-[24em] h-[10em]">
-            <p class="font-bold text-red-800">Hello World | @{{ $page.props.app_env }}</p>
+    <div class="place-items-center grid bg-red-50 h-screen">
+        <div class="place-items-center grid bg-red-100 rounded-3xl w-[24em] h-[10em]">
+            <p class="font-bold text-red-800 uppercase">Hello World | @{{ $page.props.app_env }}</p>
         </div>
     </div>
 </template>
