0149faef51551a74941385c0a0a8d3024e029a8c
daurelius <davad.tandjung@gmail.com>
2025-04-25 11:06:37 +0700

    test: setup inertia from setup-inertia branch

--- resources/js/pages/wireframe/Home.vue
+++ resources/js/pages/wireframe/Home.vue
@@ -4,7 +4,7 @@
 <template>
     <div class="place-items-center grid bg-amber-50 h-screen">
         <div class="place-items-center grid bg-amber-100 rounded-3xl w-[24em] h-[10em]">
-            <p class="font-bold text-red-800">Hello World</p>
+            <p class="font-bold text-red-800">Hello World | @{{ $page.props.app_env }}</p>
         </div>
     </div>
 </template>
